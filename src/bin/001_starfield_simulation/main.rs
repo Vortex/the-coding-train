21b8cb50b54d9373e61056a0f7dfc3f585a3b658
Peco Danajlovski <vortexmk@gmail.com>
2023-10-06 20:21:24 +0200

    preparing for repo

--- src/bin/001_starfield_simulation/main.rs
+++ src/bin/001_starfield_simulation/main.rs
@@ -1,16 +1,49 @@
 use nannou::prelude::*;
 
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 400;
+const POPULATION: u32 = 100;
+
 fn main() {
     nannou::app(model).update(update).run();
 }
 
+// #[derive(Copy)]
+struct Star {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Star {
+    fn new() -> Self {
+        Star {
+            x: random_range(-(WIDTH as f32) / 2.0, WIDTH as f32 / 2.0),
+            y: random_range(-(HEIGHT as f32) / 2.0, HEIGHT as f32 / 2.0),
+            z: random_range(0.0, WIDTH as f32 / 2.0),
+        }
+    }
+}
+
 struct Model {
+    stars: Vec<Star>,
     _window: window::Id,
 }
 
 fn model(app: &App) -> Model {
-    let _window = app.new_window().size(512, 512).view(view).build().unwrap();
-    Model { _window }
+    let _window = app
+        .new_window()
+        .size(WIDTH, HEIGHT)
+        .view(view)
+        .build()
+        .unwrap();
+
+    let mut stars = Vec::new();
+    for _ in 0..POPULATION {
+        stars.push(Star::new());
+    }
+
+    Model { stars, _window }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {
@@ -21,11 +54,18 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     // Begin drawing
     let draw = app.draw();
 
-    // Clear the background to blue.
-    draw.background().color(BLUE);
+    // Clear the background to black.
+    draw.background().color(BLACK);
 
-    // Draw a white ellipse to follow the mouse.
-    draw.ellipse().x_y(0.0, 0.0).w_h(100.0, 100.0).color(WHITE);
+    // Draw stars
+    for i in 0..POPULATION as usize {
+        let star = &_model.stars[i];
+        draw.ellipse()
+            .x_y(star.x, star.y)
+            .w_h(2.0, 2.0)
+            .color(WHITE)
+            .stroke(WHITE);
+    }
 
     // Write the result of our drawing to the window's frame.
     draw.to_frame(app, &frame).unwrap();
